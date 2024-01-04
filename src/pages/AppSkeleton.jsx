73af6a45161f159468e91d00f94563327c9a5609
Sparsh Trivedy <sparsh.trivedy2140@gmail.com>
2024-01-04 02:58:52 -0800

    replace vh with percent for mobile

--- src/pages/AppSkeleton.jsx
+++ src/pages/AppSkeleton.jsx
@@ -160,7 +160,7 @@ function AppSkeleton() {
             </Box>
             <Box
                 component="main"
-                sx={{ flexGrow: 1, width: { sm: `calc(100% - ${drawerWidth}px)` }, height: '96vh', mt: { sm: 0, xs: '3.7rem' }, bgcolor: 'background.main'}}
+                sx={{ flexGrow: 1, width: { sm: `calc(100% - ${drawerWidth}px)` }, height: {xs: `calc(100% - 3.7rem)`, sm: '96vh'}, mt: { sm: 0, xs: '3.7rem' }, bgcolor: 'background.main'}}
             >
                 <Routes>
                     <Route path="/" element={<TitleCard />} />
--- src/pages/HomeTab/Home.jsx
+++ src/pages/HomeTab/Home.jsx
@@ -4,7 +4,8 @@ import {
     Avatar, 
     Paper, 
     Fade,
-    Box
+    Box,
+    useMediaQuery
 } from '@mui/material';
 import sparsh from '../../assets/sparsh-out.jpeg';
 
@@ -12,6 +13,8 @@ export default function TitleCard()  {
     const backgroundImage = 'https://lh3.googleusercontent.com/56hiZfyb2EPY34-CEPG71OmvOGBTHLLhabrKW37cc13w4xp5cs-9Rel-zQO0anoqEejUOE6ZZPckCPVlgpG6N6rVDqoKQ7VA9iS9iHcR2jsGBDvrf0U=w2400';
 
     const [checked, setChecked] = useState(false);
+
+    const isSm = useMediaQuery((theme) => theme.breakpoints.up('sm'));
     
     useEffect(() => {
         setChecked(true)
@@ -20,7 +23,7 @@ export default function TitleCard()  {
     return (
         <Box sx={{
                 bgcolor: 'background.main', 
-                height: '100vh', 
+                height: {xs: `calc(100%)`, sm: '100vh'}, 
                 margin: 0, 
                 padding: 2, 
                 display: 'flex', 
@@ -33,7 +36,7 @@ export default function TitleCard()  {
                     backgroundImage: `url(${backgroundImage})`,
                     backgroundSize: 'cover',
                     backgroundPosition: 'center',
-                    height: '96vh',
+                    height: {xs: '100%', sm: '96vh'},
                     display: 'flex',
                     alignItems: 'center',
                     justifyContent: 'center',
@@ -43,12 +46,23 @@ export default function TitleCard()  {
                     width: '100%',
                 }}
             >
-                <Avatar alt="Sparsh Trivedy" src={sparsh} sx={{ width: '40vh', height: '40vh', marginBottom: '3vh' }} />
-                <Typography variant='h5' sx={{fontWeight: 500, color: '#212121'}}>Hello, I am</Typography>
-                <Fade in={checked} timeout={1000}>
-                    <Typography variant='h1' sx={{fontWeight: 500, textAlign: 'center', color: '#212121'}}>Sparsh Trivedy</Typography>
-                </Fade>
-                <Typography variant='h5' sx={{fontWeight: 500, color: '#212121'}}>I am a software developer</Typography>
+                <Avatar alt="Sparsh Trivedy" src={sparsh} sx={{ width: {xs: '30vh', sm: '40vh'}, height: {xs: '30vh', sm: '40vh'}, marginBottom: '3vh' }} />
+                {isSm?
+                    <Typography variant='h5' sx={{fontWeight: 500, color: '#212121'}}>Hello, I am</Typography>:
+                    <Typography variant='h5' sx={{fontWeight: 500, color: '#212121', fontSize: '1.2rem'}}>Hello, I am</Typography>
+                }
+                {isSm?
+                    <Fade in={checked} timeout={1000}>
+                        <Typography variant='h1' sx={{fontWeight: 500, textAlign: 'center', color: '#212121'}}>Sparsh Trivedy</Typography>
+                    </Fade>:
+                    <Fade in={checked} timeout={1000}>
+                        <Typography variant='h1' sx={{fontWeight: 500, textAlign: 'center', color: '#212121', fontSize: '3.5rem'}}>Sparsh Trivedy</Typography>
+                    </Fade>
+                }
+                {isSm?
+                    <Typography variant='h5' sx={{fontWeight: 500, color: '#212121'}}>I am a software developer</Typography>:
+                    <Typography variant='h5' sx={{fontWeight: 500, color: '#212121', fontSize: '1.2rem'}}>I am a software developer</Typography>
+                }
             </Paper>
         </Box>
     );
